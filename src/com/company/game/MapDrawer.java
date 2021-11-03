f7783a5da7c6b6e641ce1694c127a37bd0309e8d
David Kistauri <davidkis113@gmail.com>
2021-11-03 21:05:36 +0300

    Добавлены комментарии

--- src/com/company/game/MapDrawer.java
+++ src/com/company/game/MapDrawer.java
@@ -8,19 +8,23 @@ import java.util.concurrent.TimeUnit;
 public class MapDrawer implements Runnable {
     public synchronized void run() {
         while (Main.hero != null && !Main.monsters.isEmpty()) {
+            // Подготовка карты (очистка всех клеток)
             Main.map.prepare();
+
+            // Отрисовка гг
             Main.polygon[Main.hero.getY()][Main.hero.getX()] = '@';
+            // Отрисовка монстров
             for (Monster monster : Main.monsters) {
                 Main.polygon[monster.getY()][monster.getX()] = monster.getName().charAt(0);
             }
 
+            // Вывод карты
             for (int i = 0; i < Main.polygonSize; i++) {
                 for (int j = 0; j < Main.polygonSize; j++) {
                     System.out.print(Main.polygon[i][j]);
                 }
                 System.out.println();
             }
-            System.out.println();
             System.out.println();
 
             try {
